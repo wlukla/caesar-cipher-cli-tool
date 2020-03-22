7691145896770c19eb819daa5fd2b6868abd2d96
wlukla <vladislav.danilochkin@gmail.com>
2020-03-23 01:16:19 +0200

    fix: fix option name clashes

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const { program } = require('commander');
 
 program
+  .storeOptionsAsProperties(false)
   .requiredOption('-s, --shift <shift size>', 'a shift')
-  .requiredOption('-a <encode/decode>, --action', 'an action encode/decode')
+  .option('-a, --action <encode|decode>', 'an action encode/decode')
   .option('-i, --input <input file path>', 'an input file')
   .option('-o, --output <output file path>', 'an output file');
-
 
 console.log('Starting Cesar...');
 
 program.parse(process.argv);
 
-if (program.debug) console.log(program.opts());
-if (program.shift) console.log('shifted');
-if (program.input) console.log('input');
-if (program.output) console.log('output');
+const programOpts = program.opts();
+
+if (programOpts.shift) console.log('shifted ', programOpts.shift);
+if (programOpts.action) console.log('action');
+if (programOpts.input) console.log('input');
+if (programOpts.output) console.log('output');
