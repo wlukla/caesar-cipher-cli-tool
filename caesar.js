c32556d43243f6d35bc642ab67aac6e760434a3e
wlukla <vladislav.danilochkin@gmail.com>
2020-03-24 03:27:39 +0200

    fix: fix decrypt algorithm

--- caesar.js
+++ caesar.js
@@ -18,21 +18,8 @@ const encode = (text, shift) => {
 }
 
 const decode = (text, shift) => {
-  let res = '';
-  const key = Number(shift);
-
-  for (let i = 0; i < text.length; i += 1) {
-    if (/[a-zA-Z]/.test(text[i])) {
-      if (text[i].toUpperCase() === text[i]) {
-        res += String.fromCharCode((text.charCodeAt(i) - key - 65) % 26 + 65);
-      } else {
-        res += String.fromCharCode(Math.abs(text.charCodeAt(i) - key - 97) % 26 + 97);
-      }
-    } else {
-      res += text[i];
-    }
-  }
-
+  const newShift = (26 - shift) % 26;
+  const res = encode(text, newShift);
   return res;
 };
 
